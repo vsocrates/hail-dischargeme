8b23df9d05c785cc210f0074682b19b5484339ee
Vimig Socrates <vimig.socrates@yale.edu>
2024-03-22 12:36:32 -0400

    add exponential backoff openai func

---
+++ notebooks/brief_hospital_course/openai_utils.py
@@ -0,0 +1,5 @@
+import openai
+
+@retry(wait=wait_random_exponential(min=1, max=60), stop=stop_after_attempt(6))
+def completion_with_backoff(**kwargs):
+    return openai.ChatCompletion.create(**kwargs)
